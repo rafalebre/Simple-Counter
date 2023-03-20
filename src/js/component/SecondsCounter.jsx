f9da3840d4ba747cf4a835b94b5d931489fc3fa9
rafalebre <rafaellebre100@gmail.com>
2023-03-20 09:03:27 +0000

    Working w component

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,46 @@
+//import react into the bundle
+import { checkPropTypes, func } from "prop-types";
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+// include your styles into the webpack bundle
+import "../../styles/index.css";
+
+function SecondsCounter (props) {
+    return (<div className="bigCounter"> 
+    <div className="calendar">
+        <i className="far fa-clock"></i>
+            </div>
+    <div className="six">{props.digitSix % 10}</div>
+    <div className="five">{props.digitFive % 10}</div>
+    <div className="four">{props.digitFour % 10}</div>
+    <div className="three">{props.digitThree % 10}</div>
+    <div className="two">{props.digitTwo % 10}</div>
+    <div className="one">{props.digitOne % 10}</div>
+    </div>);
+}
+SecondsCounter.propTyper = {
+    digitSix: PropTypes.number,
+    digitFive: PropTypes.number,
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number
+};
+
+let counter = 0;
+setInterval(function(){
+    const six = Math.floor(counter/100000);
+    const five = Math.floor(counter/10000);
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    counter++;
+
+//render your react application
+ReactDOM.render(<SecondsCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six}/>, document.querySelector("#app"));
+},1000);
+
+export default SecondsCounter 
